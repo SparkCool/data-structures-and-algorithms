84a24365c2f82fec2b8ac8925630425f33f0fef3
SparkCool <52952198+SparkCool@users.noreply.github.com>
2019-08-27 23:25:09 +0800

    Create Delete_LinkList_Node.c
    查找删除单链表元素的两种方式，其本质其实是一样的。

---
+++ Link List/Delete_LinkList_Node.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+}Node, *PtrNode;
+
+void CreateList(PtrNode *head)
+{
+    *head = (PtrNode)malloc(sizeof(Node));
+    (*head)->next = NULL;
+
+    PtrNode node = *head;
+    int a[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++)
+    {
+        PtrNode s;
+        s = (PtrNode)malloc(sizeof(Node));
+
+        s->data = a[i];
+        s->next = node->next;
+        node->next = s;
+    }
+}
+
+void PrintList(PtrNode node)
+{
+    node = node->next;
+    while(node)
+    {
+        printf("%d  ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+void DeleteElem(PtrNode *head, int key, int method)
+{
+    PtrNode node;
+    node = (*head)->next;
+
+    while(node->next)
+    {
+        if (key == node->next->data)
+            break;  // 循环退出后，node是前驱节点的地址
+
+        node = node->next;
+    }
+    
+    // 方法一：本函数删除
+    if (method == 1)
+    {   
+        PtrNode q;
+        q = node->next;
+        printf("Number %d is going to be deleted\n", q->data);
+        node->next = q->next;
+        free(q);
+    }
+    // 方法二：函数调用删除
+    else if (method == 2)    
+    {
+        Delete(&(node->next));  // 因此传node传递的就是前驱节点4的next的地址，原则：修改谁就传谁的地址
+    }
+}
+
+
+void Delete(PtrNode *p)
+{
+    PtrNode q = NULL;
+    q = *p;
+    printf("Number %d is going to be deleted\n", q->data);
+    (*p) = (*p)->next;
+    free(q);
+}
+
+int main()
+{
+    PtrNode head;  // 定义头指针
+    
+    CreateList(&head);  // 在函数中使head指向内存中一个变量，要修改head的值，传入地址
+    PrintList(head);
+
+    int key = 3;
+    DeleteElem(&head, key, 2);
+    PrintList(head);
+
+    return 0;
+}
